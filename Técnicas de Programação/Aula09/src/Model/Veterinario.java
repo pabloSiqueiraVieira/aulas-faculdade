1ba0621cbffb911d724ac71110e31b3cd7c4946b
Auto Logon <autologon@CSED.LAB>
2023-04-20 20:14:29 -0300

    apenas aula de java 09 no momento

---
+++ Técnicas de Programação/Aula09/src/Model/Veterinario.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author autologon
+ */
+public class Veterinario {
+    private int id;
+    private  String nome;
+    private int crmv;
+    private String formacao;
+    
+    public int getId(){
+        return id;
+}
+    public void setId(){
+        this.id = id;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(){
+        this.nome = nome;
+    }
+    
+    public int getCrmv(){
+        return crmv;
+    }
+    
+    public void setCrmv(){
+        this.crmv = crmv;
+    }
+    
+    public String getFormacao(){
+        return nome;
+    }
+    
+    public void setFormacao(){
+        this.formacao = formacao;
+    }
+    
+    
+    
+}
+
---
+++ Técnicas de Programação/Aula09/src/aula09/Aula09.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula09;
+
+import View.TelaPrincipal;
+
+/**
+ *
+ * @author autologon
+ */
+public class Aula09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaPrincipal t1 = new TelaPrincipal();
+        t1.setVisible(true);
+    }
+    
+}
