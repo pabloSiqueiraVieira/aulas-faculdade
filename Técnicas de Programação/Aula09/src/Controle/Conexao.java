e6cafcab48d15959e0e2e648ffd55e7ca5f13a34
Auto Logon <autologon@CSED.LAB>
2023-04-20 21:02:58 -0300

    Adição de alguns features para integrar o programa com banco de dados, ainda não finalizado

---
+++ Técnicas de Programação/Aula09/src/Controle/Conexao.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controle;
+
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author autologon
+ */
+public class Conexao {
+    public Connection getConnection() throws ClassNotFoundException{
+        Connection conexao = null;
+        Class.forName = ("com.mysql.jbdc.");
+        conexao = DriverManager.getConnection("jdbc:mysql://localhost/clinica","root",null);
+        return conexao;
+    }
+    
+}
