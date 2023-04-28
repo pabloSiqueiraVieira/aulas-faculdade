aa8be1ebfb2ba2b110134a388189ec6a32a70382
Auto Logon <autologon@CSED.LAB>
2023-04-27 21:17:51 -0300

    adição da aula 10 que funciona corretamente a conexão sql, vou usar ela como base para arrumar a aula 09

---
+++ Técnicas de Programação/aula10/src/aula10/Aula10.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula10;
+
+/**
+ *
+ * @author autologon
+ */
+public class Aula10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Tela t1 = new Tela();
+        t1.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
